de4e4f5f6d47cdd52d0d84e0148e96eb60615ae0
himanshu <himanshutank1111@gmail.com>
2018-08-14 14:52:57 +0530

    ...

--- views/order-quotation/update.php
+++ views/order-quotation/update.php
@@ -4,11 +4,19 @@ use yii\helpers\Html;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\OrderQuotation */
+$type = Yii::$app->getRequest()->getQueryParam('type');
+if ($type == 'requirements') {
+    $this->title = 'Update Customer Requirements';
+    $this->params['breadcrumbs'][] = ['label' => 'Customer Requirements', 'url' => ['index?type=' . $type]];
+    $this->params['breadcrumbs'][] = ['label' => $model->order_quotation_id, 'url' => ['view?type=' . $type, 'id' => $model->order_quotation_id, 'type' => $type]];
+    $this->params['breadcrumbs'][] = 'Update';
+} else {
+    $this->title = 'Update Order Quotation';
+    $this->params['breadcrumbs'][] = ['label' => 'Order Quotations', 'url' => ['index?type=' . $type]];
+    $this->params['breadcrumbs'][] = ['label' => $model->order_quotation_id, 'url' => ['view?type=' . $type, 'id' => $model->order_quotation_id]];
+    $this->params['breadcrumbs'][] = 'Update';
+}
 
-$this->title = 'Update Order Quotation: {nameAttribute}';
-$this->params['breadcrumbs'][] = ['label' => 'Order Quotations', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->order_quotation_id, 'url' => ['view', 'id' => $model->order_quotation_id]];
-$this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="order-quotation-update">
 
